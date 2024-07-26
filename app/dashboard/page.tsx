2497ecc3c6e34df9b7ffb08ac8e6e02f73676f3f
Jose Laurito <jigolaurito93@yahoo.com>
2024-07-26 02:47:12 -0500

    suspense fallback on trends

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,21 +3,22 @@ import TransactionList from "./components/transaction-list";
 import TransactionListFallback from "./components/transaction-list-fallback";
 import Trend from "./components/trend";
 import Trending from "./components/trend";
+import TrendFallback from "./components/trend-fallback";
 
 const Dashboard = () => {
   return (
     <>
       <section className="mb-8 grid grid-cols-2 lg:grid-cols-4 gap-8">
-        <Suspense>
+        <Suspense fallback={<TrendFallback />}>
           <Trending type="Income" />
         </Suspense>
-        <Suspense>
+        <Suspense fallback={<TrendFallback />}>
           <Trending type="Expense" />
         </Suspense>
-        <Suspense>
+        <Suspense fallback={<TrendFallback />}>
           <Trending type="Investment" />
         </Suspense>
-        <Suspense>
+        <Suspense fallback={<TrendFallback />}>
           <Trending type="Saving" />
         </Suspense>
       </section>
